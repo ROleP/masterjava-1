a016699e86f9e4c0c46be6b7fab2d1f4aba27cd0
ROleP <crazy.rolep@gmail.com>
2017-05-14 12:41:10 +0300

    8-4 webapp

---
+++ web/webapp/src/main/java/ru/javaops/masterjava/webapp/SenderServlet.java
@@ -0,0 +1,30 @@
+package ru.javaops.masterjava.webapp;
+
+import lombok.extern.slf4j.Slf4j;
+import ru.javaops.masterjava.service.mail.GroupResult;
+import ru.javaops.masterjava.service.mail.MailWSClient;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by rolep on 13/05/17.
+ */
+@WebServlet("/send")
+@Slf4j
+public class SenderServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+        resp.setCharacterEncoding("UTF-8");
+        String users = req.getParameter("users");
+        String subject = req.getParameter("subject");
+        String body = req.getParameter("body");
+        GroupResult groupResult = MailWSClient.sendBulk(MailWSClient.split(users), subject, body);
+        resp.getWriter().write(groupResult.toString());
+    }
+}
