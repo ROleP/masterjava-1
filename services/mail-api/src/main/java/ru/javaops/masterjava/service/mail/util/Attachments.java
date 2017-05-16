494e983d3dc20a6f56b724c60cf20ec2f580e410
ROleP <crazy.rolep@gmail.com>
2017-05-16 15:04:23 +0300

    9-3 webapp attach

---
+++ services/mail-api/src/main/java/ru/javaops/masterjava/service/mail/util/Attachments.java
@@ -0,0 +1,49 @@
+package ru.javaops.masterjava.service.mail.util;
+
+import lombok.AllArgsConstructor;
+import ru.javaops.masterjava.service.mail.Attach;
+
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * Created by rolep on 16/05/17.
+ */
+public class Attachments {
+    public static Attach getAttach(String name, InputStream inputStream) {
+       return new Attach(name, new DataHandler(new InpustStreamDataSource(inputStream)));
+    }
+
+    @AllArgsConstructor
+    private static class InpustStreamDataSource implements DataSource {
+        private InputStream inputStream;
+
+        @Override
+        public InputStream getInputStream() throws IOException {
+            if (inputStream == null) {
+                throw new IOException("Second getInputStream() call is not supported");
+            }
+            InputStream res = inputStream;
+            inputStream = null;
+            return res;
+        }
+
+        @Override
+        public OutputStream getOutputStream() throws IOException {
+            throw new UnsupportedOperationException("Not implemented");
+        }
+
+        @Override
+        public String getContentType() {
+            return "application/octet-stream";
+        }
+
+        @Override
+        public String getName() {
+            return "";
+        }
+    }
+}
